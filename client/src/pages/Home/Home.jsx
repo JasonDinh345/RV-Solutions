e91ad8e2716ea1baf6e5580d484eb5fedc27f536
Jason Dinh <jasondinh345@gmail.com>
2025-06-03 13:58:34 -0700

    fixed typo

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -9,14 +9,14 @@ export default function Home(){
                 <h1>Purpose</h1>
                 <div id="purposeContent">
                     <p>
-                        We are developing a comprehensive database for a two-way RV marketplace catering specifically to users in Washington State. 
+                        We are developing a comprehensive database for a two-way RV marketplace.
                         This platform enables both RV owners and renters to connect, list, and book RVs efficiently. The purpose of the database is to provide a well-structured and 
                         centralized system for managing various aspects of the business, including user accounts, RV listings, bookings, and damage reports.
                     </p>
                     <br></br>
                     <p>
-                        By doing so, the business owner can ensure data consistency is upheld to prevent misdocumentation or miscommunication when creating data on thier site.
-                        With this, users can confidently interact with thier site, knowing the information they fetch/see is managed securely and accuratly. 
+                        By doing so, the business owner can ensure data consistency is upheld to prevent misdocumentation or miscommunication when creating data on their  site.
+                        With this, users can confidently interact with their site, knowing the information they fetch/see is managed securely and accurately. 
                     </p>
                 </div>
             </div>
